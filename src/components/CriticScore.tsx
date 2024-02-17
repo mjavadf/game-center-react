b5703238659e70e9c2576688bad6b926645cc103
Javad Farokhi <javad.me1376@gmail.com>
2024-02-17 21:37:36 +0100

    Show critic scores

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,12 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+  score: number;
+}
+
+function CriticScore({ score }: Props) {
+  let color = score > 75 ? "green" : score > 60 ? "yellow" : "red"
+  return <Badge fontSize={"14px"} paddingX={2} borderRadius={4} colorScheme={color}>{score}</Badge>
+}
+
+export default CriticScore;
