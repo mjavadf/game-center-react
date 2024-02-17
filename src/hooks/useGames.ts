d71eecc92fe3133687fd330353e34b3424fba361
Javad Farokhi <javad.me1376@gmail.com>
2024-02-17 21:19:57 +0100

    Show platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
-  background_image: string
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
@@ -27,7 +34,8 @@ const useGames = () => {
       })
       .catch((err) => {
         if (err instanceof CanceledError) return;
-        setError(err.message)});
+        setError(err.message);
+      });
 
     return () => controller.abort();
   }, []);
