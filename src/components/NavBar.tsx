96f534dca21c910cd9775cc4cfb2ea2572662e7b
Javad Farokhi <javad.me1376@gmail.com>
2024-03-02 18:04:49 +0100

    Improve navbar link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,9 +6,10 @@ import useGameQueryStore from "../store";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const setSearchText = useGameQueryStore(s => s.setSearchText)
   return (
     <HStack padding={"10px"}>
-      <Link to={"/"}>
+      <Link to={"/"} onClick={() => {setSearchText("")}}>
         <Image src={logo} boxSize="40px" objectFit={"cover"} />
       </Link>
       <SearchInput />
